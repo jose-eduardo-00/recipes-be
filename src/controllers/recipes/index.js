bcacb70a69650c0cd34bf6fb6abccf85f0f249de
José Eduardo Gois de Pinho Costa <eduardo@softwareprecisao.com>
2025-04-17 15:13:32 -0300

    feat: atualizado controller de criar a receita

--- src/controllers/recipes/index.js
+++ src/controllers/recipes/index.js
@@ -1,16 +1,29 @@
+import Sequelize from "../../config/database.js";
 import db from "../../models/index.js";
 
-const { Recipe, RecipeCategory } = db;
+const { Recipe, RecipeCategory, RecipeIngredients, RecipeStep } = db;
 
 export const createRecipe = async (req, res) => {
-  try {
-    const { name, desciption, userId, categorys } = req.body;
+  const transaction = await Sequelize.transaction();
 
-    const newRecipe = await Recipe.create({
+  try {
+    const {
       name,
       desciption,
       userId,
-    });
+      categorys,
+      ingredients,
+      preparationMethod,
+    } = req.body;
+
+    const newRecipe = await Recipe.create(
+      {
+        name,
+        desciption,
+        userId,
+      },
+      { transaction }
+    );
 
     if (Array.isArray(categorys) && categorys.length > 0) {
       const categoryEntries = categorys.map((categoryId) => ({
@@ -18,14 +31,40 @@ export const createRecipe = async (req, res) => {
         categoryId,
       }));
 
-      await RecipeCategory.bulkCreate(categoryEntries);
+      await RecipeCategory.bulkCreate(categoryEntries, { transaction });
     }
+
+    // Cria os ingredientes (se houver)
+    if (Array.isArray(ingredients) && ingredients.length > 0) {
+      const ingredientEntries = ingredients.map((ingredient) => ({
+        recipeId: newRecipe.id,
+        name: ingredient.name,
+        quantity: ingredient.quantity,
+      }));
+
+      await RecipeIngredients.bulkCreate(ingredientEntries, { transaction });
+    }
+
+    // Cria os métodos de preparo (se houver)
+    if (Array.isArray(preparationMethod) && preparationMethod.length > 0) {
+      const methodEntries = preparationMethod.map((step) => ({
+        recipeId: newRecipe.id,
+        description: step.description,
+        order: step.order,
+      }));
+
+      await RecipeStep.bulkCreate(methodEntries, { transaction });
+    }
+
+    await transaction.commit();
 
     res.status(201).json({
       message: "Receita criada com sucesso.",
       recipe: newRecipe,
     });
   } catch (error) {
+    await transaction.rollback();
+
     res.status(500).json({
       message: "Erro ao tentar criar a receita",
       error: error.message,
